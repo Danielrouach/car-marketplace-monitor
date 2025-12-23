8da0efe1e8fdd91fcbd8a251bf7525c574763a4e
Danielrouach <Danielrouach1@gmail.com>
2025-12-23 09:35:14 +0200

    Delete # core directory

--- # core/main.py
+++
@@ -1,8 +0,0 @@
-from core.orchestrator import run
-
-requirements = {
-    "model": "mazda",
-    "max_price": 45000
-}
-
-run(requirements, "user@email.com")
--- # core/matches_requirements.py
+++
@@ -1,6 +0,0 @@
-# core/filters.py
-def matches_requirements(car, req):
-    return (
-        car.price <= req["max_price"]
-        and req["model"].lower() in car.title.lower()
-    )
