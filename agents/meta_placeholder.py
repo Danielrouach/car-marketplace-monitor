34a61afe5526462cb6eafcbc9e912bde57c942bc
Danielrouach <Danielrouach1@gmail.com>
2025-12-23 09:38:16 +0200

    Add MetaPlaceholderAgent class for Meta integration

---
+++ agents/meta_placeholder.py
@@ -0,0 +1,9 @@
+class MetaPlaceholderAgent:
+    """
+    Placeholder for Facebook / Instagram integration.
+
+    IMPORTANT:
+    Implementation should be done ONLY using official Meta Graph API.
+    Scraping or automated login is intentionally avoided.
+    """
+    pass
